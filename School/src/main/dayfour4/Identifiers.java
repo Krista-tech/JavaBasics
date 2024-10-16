83127fc70c53028c052a6a435a38d023f95ef609
krista.bokanova <krista.bokanova@accenture.com>
2024-10-16 23:14:13 +0300

    task update

---
+++ School/src/main/dayfour4/Identifiers.java
@@ -0,0 +1,20 @@
+package main.dayfour4;
+
+public class Identifiers {
+    public static void main(String[] args) {
+
+        // Good
+        int minutesPerHour = 60;
+
+        // OK, but not so easy to understand what m actually is
+        int m = 60;
+
+
+//        Names can contain letters, digits, underscores, and dollar signs
+//        Names must begin with a letter
+//        Names should start with a lowercase letter, and cannot contain whitespace
+//        Names can also begin with $ and _
+//        Names are case-sensitive ("myVar" and "myvar" are different variables)
+//        Reserved words (like Java keywords, such as int or boolean) cannot be used as names
+    }
+}
---
+++ School/src/main/dayfour4/MultipleVariables.java
@@ -0,0 +1,22 @@
+package main.dayfour4;
+
+public class MultipleVariables {
+    public static void main(String[] args) {
+
+        // Declare multiple variables of the same type on separate lines
+        int a = 5;
+        int b = 6;
+        int c = 50;
+        // Print the sum of the variables
+        System.out.println(a + b + c);
+
+
+        // Declare multiple variables of the same type on the same line
+        int x = 5, y = 10, z = 15;
+
+        // Print the values of the variables
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("z: " + z);
+    }
+}
